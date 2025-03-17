8db745ba0109bad033e765dfbfe08eb80ad57adf
jpgits <afjp58@gmail.com>
2025-03-17 17:20:07 +0900

    Update and rename main.js to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const heading = document.querySelector('header > h1');
+  heading.addEventListener('click', () => {
+    alert('ようこそ・ポートフォリオサイトへ、ごみです。');
+  });
+});
